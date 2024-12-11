cce3b6a0f397f5b57710204e7e949f1dede844e0
pavan-klbg <allurpavan111@gmail.com>
2024-12-11 23:49:13 +0530

    svc registry remote push

---
+++ ServiceRegistry/src/test/java/com/micro/serviceRegistry/ServiceRegistry/ServiceRegistryApplicationTests.java
@@ -0,0 +1,14 @@
+package com.micro.serviceRegistry.ServiceRegistry;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+@SpringBootTest
+class ServiceRegistryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
